61556583128b175286b0e88f8d8a44de6be705e5
Leandro Toledo de Souza <leandrotoledo@toddbook02.todd.lan>
2011-10-08 16:35:08 -0300

    Modulando app blog 1/2

---
+++ blog/urls.py
@@ -0,0 +1,64 @@
+from django.conf.urls.defaults import patterns, include, url
+from django.views.generic import DetailView, ListView, TemplateView, RedirectView
+from django.views.generic.dates import DayArchiveView, MonthArchiveView, YearArchiveView
+from django.contrib.sitemaps import GenericSitemap
+from .models import *
+from .views import *
+
+info_dict = {
+    'queryset': Post.objects.all(),
+    'date_field': 'updated_date'
+}
+
+sitemaps = {
+    'blog': GenericSitemap(info_dict, priority=1.0)
+}
+
+urlpatterns = patterns('',
+    (r'^comments/', include('django.contrib.comments.urls')),
+
+    url(r'^$',
+        PostListView.as_view(),
+        name='index'),
+
+    (r'^posts/$',
+        RedirectView.as_view(url='/')),
+
+    (r'^categories/$',
+        RedirectView.as_view(url='/')),
+
+    (r'^pages/$',
+        RedirectView.as_view(url='/')),
+
+    url(r'^categories/(?P<category>[-\w]+)/$',
+        PostCategoryListView.as_view(),
+        name='category'),
+
+    url(r'^users/(?P<user>[-\w]+)/$',
+        PostUserListView.as_view(),
+        name='user'),
+
+    url(r'^posts/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)/$',
+        PostDetailView.as_view(),
+        name='post'),
+
+    url(r'^posts/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/$',
+        PostDayArchiveView.as_view(),
+        name='archive_day'),
+
+    url(r'^posts/(?P<year>\d{4})/(?P<month>\d{1,2})/$',
+        PostMonthArchiveView.as_view(),
+        name='archive_month'),
+
+    url(r'^posts/(?P<year>\d{4})/$',
+        PostYearArchiveView.as_view(),
+        name='archive_year'),
+
+    url(r'^pages/(?P<slug>[-\w]+)/$',
+        PageDetailView.as_view(),
+        name='page'),
+
+    (r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps})
+
+    # Criar redirect para compatibilidade com blog antigo
+)
